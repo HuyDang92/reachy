a1fd3aa202118d7b12b775894011023d89a318c0
PhuocKhangNguyen <khanglegendneverdie@gmail.com>
2022-11-10 14:25:50 +0700

    gt

--- site/homepage/contact.php
+++ site/homepage/contact.php
@@ -5,11 +5,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Liên hệ</title>
+    <link rel="stylesheet" href="<?= $CONTENT_URL ?>/css/form.css">
+    <link rel="stylesheet" href="<?= $CONTENT_URL ?>/css/root.css">
 </head>
 
 <body>
-    <h1>Liên hệ</h1>
+    <div class="background_header">
+        <img style="width: 100%; height: 50%;" src="<?= $CONTENT_URL ?>/imgs/interface/background.png" alt="">
+    </div>
+    <div class="container__sign-in">
+        <div class="title__sign-in">
+            <h1>Liên hệ</h1>
+            <div class="title_link">
+                <a href="<?= $SITE_URL ?>/homepage">Home</a> <i class="fa-solid fa-arrow-right-long"></i> Liên hệ
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
--- site/homepage/introduce.php
+++ site/homepage/introduce.php
@@ -5,11 +5,82 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Giới thiệu</title>
+    <link rel="stylesheet" href="<?= $CONTENT_URL ?>/css/form.css">
+    <link rel="stylesheet" href="<?= $CONTENT_URL ?>/css/root.css">
+    <link rel="stylesheet" href="<?= $CONTENT_URL ?>/css/introduce.css">
 </head>
 
 <body>
-    <h1>Giới thiệu</h1>
+    <div class="background_header">
+    <img style="width: 100%; height: 50%;" src="<?= $CONTENT_URL ?>/imgs/interface/background.png" alt="">
+    </div>
+    <div class="container__sign-in">
+        <div class="title__sign-in">
+            <h1>Giới thiệu</h1>
+            <div class="title_link">
+                <a href="<?= $SITE_URL ?>/homepage">Home</a> <i class="fa-solid fa-arrow-right-long"></i> Giới thiệu
+            </div>
+        </div>
+    </div>   
+    <h1 class="title_h1">TẦM NHÌN - SỨ MỆNH REACHY: NÂNG TẦM THƯƠNG HIỆU VIỆT</h1> 
+    <div class="img_introduce">
+        <img  src="<?=$CONTENT_URL?>/imgs/interface/GT1.png" alt="enterprise"><br>
+    </div>
+    <div class="container_introduce">
+        <span>Bắt đầu từ thương hiệu thời trang Hi5 ra đời trong năm 2009, trải qua chặng đường phát triển đầy khó khăn, Hi5 được đổi tên thành Reachy vào năm 2014 với ước mơ gây dựng một thương hiệu thời trang hàng đầu thế giới.</span>
+        <br><br> <span>Từ đó trở đi Reachy lớn mạnh không ngừng, đến năm 2016 Reachy đã có 38 cửa hàng, chỉ sau 2 năm vào năm 2018 Reachy đã có 73 cửa hàng. Đến năm 2019 Reachy đã có 82 cửa hàng và tính đến thời điểm hiện tại Reachy đã mở rộng được hơn 100 cửa hàng trên toàn quốc.</span>
+        <br><br><br> <h3>NIỀM TIN CỦA REACHY</h3>
+        <br>
+        <ul>
+            <li>Tất cả các khoản chi đều là chi phí, chỉ có chi cho khách hàng và nhân viên là không phí.</li>
+            <li>Tất cả những thành viên của Reachy đều nỗ lực hết sức và có năng lực để thực hiện mục tiêu.</li>
+            <li>Mỗi thành viên Reachy đều có thể thay đổi khi được trao niềm tin, ghi nhận, hướng dẫn và đào tạo.</li>
+        </ul>
+        <br><h3>SỨ MỆNH CỦA REACHY</h3>
+        <span>Sứ mệnh của Reachy là "Đưa sản phẩm thời trang Việt có chất liệu tốt, dịch vụ tốt đến tận tay khách hàng tại các vùng miền Việt Nam và trên Thế Giới".</span>
+        <span>Reachy mong muốn mang đến cho toàn bộ khách hàng trên khắp mọi miền tổ quốc Việt Nam những sản phẩm thời trang do chính tay người Việt làm ra. Không phân biệt tầng lớp, không phân biệt giàu nghèo, những khách hàng chưa bao giờ được trải nghiệm dịch vụ mua sắm vượt ngoài mong đợi, ai cũng sẽ được chào đón, tôn trọng khi đến với Reachy.</span>
+        <span>Chính vì vậy, Reachy dày công nghiên cứu chất liệu sản phẩm và cho ra mắt những dòng sản phẩm tối ưu cả về giá cả và chất lượng mang đến cho khách hàng. Cùng với đó, Reachy luôn dành phần lớn thời gian để đào tạo văn hóa phục vụ cho toàn bộ nhân viên. Mỗi nhân viên sẽ là 1 đại sứ thương hiệu, mỗi nhân viên sẽ là 1 hình mẫu về văn hóa phục vụ của Reachy và trao giá trị tốt nhất đến từng khách hàng.</span>
+        <h3>GIÁ TRỊ CỐT LÕI CỦA REACHY</h3>
+        <span>Ở Reachy, chúng tôi luôn gìn giữ 6 Giá Trị Cốt Lõi để mọi bước đi đều thêm vững chắc và giàu giá trị.</span><br>
+        <b>1. Đam mê phục vụ khách hàng</b>
+        <ul>
+            <li>Luôn đặt sự hài lòng của khách hàng là ưu tiên số 1 trong mọi suy nghĩ và hành động của mình.</li>
+            <li>Luôn Chủ động, Cười, Chào, Cảm ơn.</li>
+        </ul>
+        <b>2. Coi mình là gốc rễ của mọi vấn đề</b>
+        <ul>
+            <li>Là một sự lựa chọn để từ đó bạn có sức mạnh để thay đổi kết quả.</li>
+            <li>Là bạn đã từ bỏ quyền coi mình là nạn nhân (Từ bỏ quyền đổ lỗi, chỉ trích, than phiền).</li>
+        </ul>
+        <b>3. Chính trực</b>
+        <ul>
+            <li>Hoàn tất lời hứa của mình.</li>
+            <li>Làm đúng, làm đủ theo thiết kế</li>
+            <li>Khi đã nỗ lực hết sức mà thấy nguy cơ không thể giữ lời thì ngay lập tức thông tin cho những người có liên quan, tìm hiểu tác động, dọn dẹp hậu quả và đưa ra lời hứa mới.</li>
+        </ul>
+        <b>4. Trung thực</b>
+        <ul>
+            <li>Không lấy, không sử dụng và không nghĩ đến việc chiếm hữu tiền bạc, hàng hoá, tài sản không phải của mình.</li>
+            <li>Nói thẳng, nói thật một cách kịp thời và trực tiếp.</li>
+            <li>Sẵn lòng đóng góp và đón nhận sự đóng góp.</li>
+            <li>Báo cáo đúng, đủ trạng thái công việc.</li>
+        </ul>
+        <b>5. Cầu tiến</b>
+        <ul>
+            <li>Sẵn sàng thử nghiệm phương pháp mới, chấp nhận thất bại và tiếp tục hành động.</li>
+            <li>Luôn "Say YES", hành động quyết liệt và tin tưởng sẽ đạt được mục tiêu thách thức.</li>
+        </ul>
+        <b>6. Yêu thương và hỗ trợ đồng đội</b>
+        <ul>
+            <li>Sẵn lòng hỗ trợ, hướng dẫn, động viên để họ hoàn thành tốt công việc. Không làm thay, không bao che.</li>
+            <li>Đứng vào vị trí của người nói để lắng nghe đầy đủ và không thành kiến.</li>
+            <li>Thẳng thắn theo hướng đóng góp xây dựng. Luôn ghi nhận thành quả hoặc nỗ lực dù chưa có thành quả.</li>
+        </ul>
+        <h3>TẦM NHÌN CỦA REACHY</h3>
+        <span>2025: Công ty thời trang số 1 Việt Nam. IPO và trở thành "Kỳ lân" tiếp theo của Việt Nam. Cộng đồng Reachy có cuộc sống viên mãn, hạnh phúc.</span>
+        <span>2038: Công ty thời trang số 1 Thế Giới. Cộng đồng Reachy có cuộc sống viên mãn, hạnh phúc.</span>
+    </div>
 </body>
 
 </html>
