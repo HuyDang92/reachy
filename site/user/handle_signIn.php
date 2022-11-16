ae480c27431cd8874f007d817bd1e3eac48e848a
HuyDang92 <95539752+HuyDang92@users.noreply.github.com>
2022-11-16 10:38:39 +0700

    update

--- site/user/handle_signIn.php
+++ site/user/handle_signIn.php
@@ -1,25 +1,27 @@
-<?php  
+<?php
 session_start();
 
-    require_once "../../global.php";
-    require_once "../../dao/pdo.php";
-    require_once "../../dao/user.php";
-    session_start();
-    extract($_REQUEST);
-    if(!user_checkExistEmail($email)){
+require_once "../../global.php";
+require_once "../../dao/pdo.php";
+require_once "../../dao/user.php";
+require_once "../../dao/brand.php";
+require_once "../../dao/product.php";
+require_once "../../dao/comment.php";
+extract($_REQUEST);
+if (!user_checkExistEmail($email)) {
+    global $email;
+    $MESSAGE = "Địa chỉ email không tồn tại";
+    $VIEW_NAME = "user/sign-in.php";
+} else {
+    $info = user_selectByEmail($email);
+    if ($info['password'] != $password) {
         global $email;
-        $MESSAGE = "Địa chỉ email không tồn tại";
+        $MESSAGE = "Mật khẩu không đúng";
         $VIEW_NAME = "user/sign-in.php";
-    }else{
-        $info = user_selectByEmail($email);
-        if($info['password'] != $password){
-            global $email;
-            $MESSAGE = "Mật khẩu không đúng";
-            $VIEW_NAME = "user/sign-in.php";
-            $check = false;
-        }else{
-            user_signIn($email,$password);
-            header("location:../../index.php");
-        }
+        $check = false;
+    } else {
+        user_signIn($email, $password);
+        header("location:../../index.php");
     }
-    require "../layout-overall.php";
+}
+require "../layout-overall.php";
