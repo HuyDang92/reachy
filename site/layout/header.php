e797d12b5bf7160f11290c98ebda0ec423b53884
HuyDang92 <95539752+HuyDang92@users.noreply.github.com>
2022-11-29 22:27:07 +0700

    tablet

--- site/layout/header.php
+++ site/layout/header.php
@@ -10,7 +10,7 @@ if (isset($_SESSION['login'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= $CONTENT_URL ?>/css/site_css/header.css">
-    <link rel="stylesheet" href="<?= $CONTENT_URL ?>/css/site_css/header-mobile.css">
+    <link rel="stylesheet" href="<?= $CONTENT_URL ?>/css/site_css/mobile.css">
     <link rel="stylesheet" href="<?= $CONTENT_URL ?>/css/root.css">
     <?php if (exist_param("contact")) { ?>
     <style>
